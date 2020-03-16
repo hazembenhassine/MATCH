bf9dd4bb2f01a7dfc5f28a37ef6d1181cb322c55
hazembenhassine <MedHazem.BenHassine@insat.u-carthage.tn>
2020-03-16 21:35:15 +0100

    Finalized

--- src/app/models/Business.ts
+++ src/app/models/Business.ts
@@ -8,4 +8,10 @@ export interface Business {
   __v: number;
   logo_img: string;
   stand_img: string;
+  tel1: string;
+  tel2: string;
+  facebook_url: string;
+  linkedin_url: string;
+  twitter_url: string;
+  address: string;
 }
